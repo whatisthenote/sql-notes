aa630f5bf5a9c47724949e91635ba28fdbb4b054
whatisthenote <jgj_junior@yahoo.com.br>
2020-02-01 21:18:31 -0300

    register endpoint

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mysql = require("mysql");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -12,8 +13,13 @@ const connection = mysql.createConnection({
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000);
+
 app.get("/", (req, res) => {
   const q = "select count(*) as count from users";
+
   connection.query(q, (err, results) => {
     if (err) throw error;
     const count = results[0].count;
@@ -22,4 +28,10 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(3000);
+app.post("/register", (req, res) => {
+  const person = { email: req.body.email };
+  connection.query("insert into users set ?", person, (err, res) =>
+    console.log(res)
+  );
+  res.redirect("/");
+});
